206f11f801e7397023a327ceeefe888ff53a31af
asharma1990 <atulsharma1990@outlook.com>
2017-02-21 19:10:48 +0530

    Resume website

---
+++ WebApplication1/js/app.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="libs/jquery-3.1.1.intellisense.js" />
+//'use strict';
+
+//var blogApp = angular.module('blogApp', [
+//    'ngRoute',
+//    'blogControllers'
+//]);
+
+//blogApp.config(['$routeProvider','$locationProvider',
+//    function($routeProvider, $locationProvider){
+//        $routeProvider.
+//            when('/', {
+//                templateUrl: 'partials/main.html',
+//                controller: 'BlogCtrl'
+//            }).when('/blogPost/:_id', {
+//                templateUrl: 'partials/blogPost.html',
+//                controller: 'BlogViewCtrl'
+//            });
+//        $locationProvider.html5Mode(false).hashPrefix('!');
+//    }]);
+console.log('Start');
+$(document).ready(function () {
+    console.log('infunc');
+    $('a').on('click', function (event) {
+        console.log('inClickfunc');
+        if (this.hash !== "") {
+            console.log('insideif');
+            event.preventDefault();
+            var target = this.hash;
+            console.log(target);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            console.log(target);
+            $('html,body').animate({ scrollTop: target.offset().top }, 800);
+            };
+        });
+    });
---
+++ WebApplication1/js/controller.js
@@ -0,0 +1,64 @@
+﻿/// <reference path="libs/angular.min.js" />
+'use strict';
+
+var blogControllers = angular.module('blogControllers', []);
+
+blogControllers.controller('BlogCtrl', ['$scope',
+    function BlogCtrl($scope) {
+        $scope.blogArticle = "This is a blog post about Angular JS. We will cover how to build a blog and how to add comments to the blog post.";
+        $scope.blogList = [
+            {
+                "_id": 1,
+                "date": 1400623623107,
+                "introText": "This is Intro of Blog 1",
+                "blogText": "This is blog text of Blog 1."
+            },
+            {
+                "_id": 2,
+                "date": 1400267723107,
+                "introText": "This is Intro of Blog 2",
+                "blogText": "This is blog text of Blog 2."
+            }
+        ];
+    }]);
+
+blogControllers.controller('BlogViewCtrl', ['$scope','$routeParams',
+    function BlogViewCtrl($scope, $routeParams) {
+
+        var blogId = $routeParams._id;
+        var blog1= {
+            "_id": 1,
+            "date": 1400623623107,
+            "introText": "This is Intro of Blog 1",
+            "blogText": "This is blog text of Blog 1.",
+            "comments":[
+                {
+                    "commentText":"Comment1"
+                },
+                {
+                    "commentText":"Comment2"
+                }
+            ]
+        };
+        var blog2 = {
+            "_id": 2,
+            "date": 1400267723107,
+            "introText": "This is Intro of Blog 2",
+            "blogText": "This is blog text of Blog 2.",
+            "comments": [
+                {
+                    "commentText": "Comment1"
+                },
+                {
+                    "commentText": "Comment2"
+                }
+            ]
+        };
+
+        if (blogId === '1') {
+            $scope.blogEntry = blog1;
+        } else if (blogId === '2') {
+            $scope.blogEntry = blog2;
+        }
+        
+    }]);
